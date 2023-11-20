e4865a53b95a0c047686410118c5d841097e6760
Aiman Syakirin <98005633+sykrnmhd@users.noreply.github.com>
2023-11-21 00:55:04 +0800

    Draft 1
    Updated

--- web/main.dart
+++ web/main.dart
@@ -1,82 +1,8 @@
 // main.dart
 import 'dart:html';
-import 'dart:convert';
+import 'payroll_class.dart';
 
-class PayrollManagementSystem {
-  Map<int, double> workingHoursMap = {};
-  int accumulationCount = 0;
 
-  double hourlyRate = 6.0; // RM per hour
-  double epfContributionRate = 0.11; // 11% EPF contribution rate
-
-  void recordWorkingHours(int? employeeId, double? hours) {
-    if (employeeId != null && hours != null) {
-      workingHoursMap[employeeId] ??= 0.0;
-      workingHoursMap[employeeId] =
-          (workingHoursMap[employeeId] ?? 0.0) + hours;
-      print(
-          "Working hours recorded for Employee $employeeId: ${workingHoursMap[employeeId]} hours");
-    } else {
-      print("Invalid input. Please provide both Employee ID and Hours Worked.");
-    }
-  }
-
-  void calculateSalary(int? employeeId) {
-    if (employeeId != null) {
-      double hoursWorked = workingHoursMap[employeeId] ?? 0.0;
-      double grossSalary = hoursWorked * hourlyRate;
-      double epfContribution = grossSalary * epfContributionRate;
-      double netSalary = grossSalary - epfContribution;
-
-      print("Employee $employeeId Salary Details:");
-      print("Hours Worked: $hoursWorked hours");
-      print("Gross Salary (Before EPF): RM $grossSalary");
-      print("EPF Contribution (11%): RM $epfContribution");
-      print("Net Salary (After EPF): RM $netSalary");
-
-      // Display the result in the UI
-      var resultGrossElement = querySelector('#resultGross');
-      resultGrossElement?.text =
-          "Gross Salary for Employee $employeeId: RM $grossSalary";
-
-      var resultNetElement = querySelector('#resultNet');
-      resultNetElement?.text =
-          "Net Salary for Employee $employeeId: RM $netSalary";
-
-      // Automatically reset after 22 accumulations
-      accumulationCount++;
-      if (accumulationCount >= 22) {
-        resetWorkingHours();
-      }
-    } else {
-      print(
-          "Invalid input. Please provide Employee ID for salary calculation.");
-    }
-  }
-
-  void resetWorkingHours() {
-    workingHoursMap.clear();
-    accumulationCount = 0;
-    print("Working hours reset.");
-  }
-
-  void saveToLocalStorage() {
-    window.localStorage['workingHoursMap'] = json.encode(workingHoursMap);
-    window.localStorage['accumulationCount'] = json.encode(accumulationCount);
-  }
-
-  void loadFromLocalStorage() {
-    var storedData = window.localStorage['workingHoursMap'];
-    if (storedData != null) {
-      workingHoursMap = Map<int, double>.from(json.decode(storedData));
-    }
-
-    var countData = window.localStorage['accumulationCount'];
-    if (countData != null) {
-      accumulationCount = json.decode(countData);
-    }
-  }
-}
 
 void main() {
   var payrollSystem = PayrollManagementSystem();
@@ -88,13 +14,18 @@ void main() {
 
   if (recordButton != null) {
     recordButton.onClick.listen((_) {
+  
       var employeeIdInput = querySelector('#employeeId') as InputElement?;
-      var hoursInput = querySelector('#hoursWorked') as InputElement?;
+      var timeInInput = querySelector('#timeIn') as InputElement?;
+      var timeOutInput = querySelector('#timeOut') as InputElement?;
 
       int? employeeId = employeeIdInput?.valueAsNumber?.toInt();
-      double? hours = hoursInput?.valueAsNumber?.toDouble();
+      String? checkInTime = timeInInput?.value;
+      String? checkOutTime = timeOutInput?.value;
 
-      payrollSystem.recordWorkingHours(employeeId, hours);
+      payrollSystem.recordWorkingHours(employeeId, checkInTime, checkOutTime);
+
+
     });
   }
 
---
+++ web/payroll_class.dart
@@ -0,0 +1,72 @@
+import 'dart:html';
+import 'dart:convert';
+
+class PayrollManagementSystem {
+  Map<int, double> workingHoursMap = {};
+  int accumulationCount = 0;
+  
+  double hourlyRate = 6.0; // RM per hour
+  double epfContributionRate = 0.11; // 11% EPF contribution rate
+
+  void recordWorkingHours(int? employeeId, String? checkInTime, String? checkOutTime) {
+    if (employeeId != null && checkInTime != null && checkOutTime != null) {
+      DateTime checkIn = DateTime.parse('1970-01-01 $checkInTime:00Z');
+      DateTime checkOut = DateTime.parse('1970-01-01 $checkOutTime:00Z');
+      double hours = checkOut.difference(checkIn).inMinutes / 60.0;
+
+      workingHoursMap[employeeId] = 
+          (workingHoursMap[employeeId] ?? 0.0) + (hours);
+    }
+
+    // Automatically reset after 22 accumulations
+      accumulationCount++;
+      if (accumulationCount >= 22) {
+        resetWorkingHours();
+      }
+  }
+
+  void calculateSalary(int? employeeId) {
+    if (employeeId != null) {
+      double hoursWorked = workingHoursMap[employeeId] ?? 0.0;
+      double grossSalary = hoursWorked * hourlyRate;
+      double epfContribution = grossSalary * epfContributionRate;
+      double netSalary = grossSalary - epfContribution;
+
+      // Display the result in the UI
+      var resultGrossElement = querySelector('#resultGross');
+      resultGrossElement?.text =
+          "Gross Salary for Employee $employeeId: RM ${grossSalary.toStringAsFixed(2)}";
+
+      var resultNetElement = querySelector('#resultNet');
+      resultNetElement?.text =
+          "Net Salary for Employee $employeeId: RM ${netSalary.toStringAsFixed(2)}";
+
+      var accCount = querySelector('#accCount');
+      accCount?.text = "Days Worked for the month: $accumulationCount" + "/22";
+    }
+  }
+
+  void resetWorkingHours() {
+    workingHoursMap.clear();
+    accumulationCount = 0;
+    print("Working hours reset.");
+  }
+
+  void saveToLocalStorage() {
+    window.localStorage['workingHoursMap'] = json.encode(workingHoursMap);
+    window.localStorage['accumulationCount'] = json.encode(accumulationCount);
+  }
+
+  void loadFromLocalStorage() {
+    var storedData = window.localStorage['workingHoursMap'];
+    if (storedData != null) {
+      workingHoursMap = Map<int, double>.from(json.decode(storedData));
+    }
+
+    var countData = window.localStorage['accumulationCount'];
+    if (countData != null) {
+      accumulationCount = json.decode(countData);
+    }
+  }
+
+}
