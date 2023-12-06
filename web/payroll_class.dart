9ca1feaaa2b0e249c3aa065dfebb1e4272a035e8
Azam Al Deen <122336794+AzamAlDeen@users.noreply.github.com>
2023-12-06 20:23:57 +0800

    payroll_class.dart

--- web/payroll_class.dart
+++ web/payroll_class.dart
@@ -30,28 +30,47 @@ class PayrollManagementSystem {
     }
   }
 
-  
-
   void calculateSalary(int? employeeId) {
-    if (employeeId != null) {
-      double hoursWorked = workingHoursMap[employeeId] ?? 0.0;
-      double grossSalary = hoursWorked * hourlyRate;
-      double epfContribution = grossSalary * epfContributionRate;
-      double netSalary = grossSalary - epfContribution;
+  if (employeeId != null) {
+    double hoursWorked = workingHoursMap[employeeId] ?? 0.0;
+    double grossSalary = hoursWorked * hourlyRate;
+    double epfContribution = grossSalary * epfContributionRate;
+    double netSalary = grossSalary - epfContribution;
 
-      // Display the result in the UI
-      var resultGrossElement = querySelector('#resultGross');
-      resultGrossElement?.text =
-          "Gross Salary for Employee $employeeId: RM ${grossSalary.toStringAsFixed(2)}";
+    // Create a new dialog element
+    var dialog = DivElement()
+      ..id = 'dialog'
+      ..classes.add('dialog');
 
-      var resultNetElement = querySelector('#resultNet');
-      resultNetElement?.text =
-          "Net Salary for Employee $employeeId: RM ${netSalary.toStringAsFixed(2)}";
+    // Create the dialog content
+    var dialogContent = DivElement()
+      ..classes.add('dialog-content');
+    dialog.append(dialogContent);
 
-      var accCount = querySelector('#accCount');
-      accCount?.text =
-          "Days Worked for Employee $employeeId: ${accumulationCountMap[employeeId] ?? 0}";
-    }
+    // Create the text elements
+    var resultGrossElement = ParagraphElement()
+      ..text = "Gross Salary for Employee $employeeId: RM ${grossSalary.toStringAsFixed(2)}";
+    dialogContent.append(resultGrossElement);
+
+    var resultNetElement = ParagraphElement()
+      ..text = "Net Salary for Employee $employeeId: RM ${netSalary.toStringAsFixed(2)}";
+    dialogContent.append(resultNetElement);
+
+    var accCount = ParagraphElement()
+      ..text = "Days Worked for Employee $employeeId: ${accumulationCountMap[employeeId] ?? 0}";
+    dialogContent.append(accCount);
+
+    // Create the close button
+    var closeButton = ButtonElement()
+      ..classes.add('close-button')
+      ..text = 'Close'
+      ..onClick.listen((_) {
+        dialog.remove(); // Remove the dialog when the close button is clicked
+      });
+    dialogContent.append(closeButton);
+
+    document.body?.append(dialog); // Add the dialog to the body of the page
+  }
   }
 
   void resetWorkingHours(int? employeeId) {
